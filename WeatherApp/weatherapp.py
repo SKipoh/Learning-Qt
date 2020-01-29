c37a6b5e95a6538a2a58e287d90ae0a618dafefb
Jack Burne <jack_burne@yahoo.co.uk>
2020-01-29 23:18:33 +0000

    Added the layout for the search bar that will sit at the top of the screen

--- WeatherApp/weatherapp.py
+++ WeatherApp/weatherapp.py
@@ -1,6 +1,25 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVboxLayout, QLabel
+from PyQt5.QtWidgets import *
 
 # Creating the QApplication called "app"
 app = QApplication([])
-
+# Creating a window to display our current and forecast weather
 window = QWidget()
+
+# Creating a layout to hold the searchbar and search button
+searchBarLayout = QHBoxLayout()
+# Creating a search bar for the user to search for their city
+searchBarLayout.addWidget(QLineEdit(placeholderText="Please Enter Your City..."))
+searchBarLayout.addWidget(QPushButton("Search"))
+
+# Creating the mainLyout as a Vertical layout
+mainLayout = QVBoxLayout()
+mainLayout.addLayout(searchBarLayout)
+
+# Setting our window's main layout to be "mainLayout". This contains all our
+# sub-layers that hold the searchbar, forecast and the detailed information
+# of the day's forecast
+window.setLayout(mainLayout)
+
+window.show()
+
+app.exec_()
