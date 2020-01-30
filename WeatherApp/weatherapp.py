fcaa4a46dad033e5c3efef2d10ecaf691e151b2f
Jack Burne <jack_burne@yahoo.co.uk>
2020-01-30 00:46:56 +0000

    Moved around the code that creates the UI to it's own method and added a title

--- WeatherApp/weatherapp.py
+++ WeatherApp/weatherapp.py
@@ -5,7 +5,11 @@ from searchbar import searchBar
 class mainWindow(QWidget):
     def __init__(self):
         super(mainWindow, self).__init__()
+        self.title = "Weather App"
+        self.setWindowTitle(self.title)
+        self.initUi()
 
+    def initUi(self):
         main_layout = QVBoxLayout()
 
         self.searchbar_layer = searchBar(self)
