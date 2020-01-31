596cdce745c17e60a98fe5c57f1a3997d4ad075c
Jack Burne <jack_burne@yahoo.co.uk>
2020-01-31 21:07:50 +0000

    Added tiles class for each forecast day

--- WeatherApp/forecast.py
+++ WeatherApp/forecast.py
@@ -1,6 +1,18 @@
-from PyQt5 import QHBoxLayout, QLabel
+from PyQt5 import QHBoxLayout, QLabel, QVBoxLayout
+
 
 class forecast(QHBoxLayout):
     def __init__(self, parent, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
         self.parent = parent
+
+
+
+class tile(QVBoxLayout):
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+        self.parent = parent
+
+        self.timeText = ""
+
+        self.addwidget(QLabel())
